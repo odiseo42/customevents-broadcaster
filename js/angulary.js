6159c4f263e902cc55dcce79ba97b7ff6bf245ef
Erick Mendoza <erick@EricksMBP.local>
2013-11-12 12:45:41 -0600

    Functional demo of how to use CustomEvents to broadcast messages
    and a smitten jquery extension to broadcast such events

---
+++ js/angulary.js
@@ -0,0 +1,104 @@
+var $scope;
+var app = angular.module('miniapp', []);
+app.directive('editable', function() {
+    return {
+        restrict: 'A',
+        scope: {field: '='},
+        replace: false,
+        template:
+          '<span>'+
+              '<input type="text" ng-model="field" ng-show="edit" ng-enter="edit=false"></input>'+
+              '<span ng-show="!edit">{{field}}</span>'+
+          '</span>',
+        link: function(scope, element, attrs) {
+            scope.edit = false;
+            $(element).bind('click', function() {
+              //console.log('clicked on '+scope.field);
+              // create and dispatch the event
+              var wiserEvent = new CustomEvent("wiserEvent",
+                {
+                  "detail":
+                  {
+                    "eventType": "Editing to-do item",
+                    "item": scope.field
+                  },
+                bubbles: false
+              });
+              $('body').broadcast(wiserEvent);
+
+              scope.$apply(scope.edit = true);
+            });
+        }
+    };
+});
+
+app.directive('listenForCustomEvents', function() {
+    return {
+        restrict: 'A',
+        replace: false,
+        link: function(scope, element, attrs) {
+            scope.edit = false;
+
+            var notif_area = $(element);
+            element[0].addEventListener("wiserEvent", function(e) {
+
+              notif_area.fadeIn(1, function(){
+                notif_area.html('I heard something: ' +e.detail.eventType + ', ['+ e.detail.item+']');
+              }).delay(1000).fadeOut(function(){
+                notif_area.html('');
+              });
+
+              console.log("Angular Listened to wiserEvent: ", e);
+            });
+        }
+    };
+});
+
+app.directive('ngEnter', function() {
+    return function(scope, element, attrs) {
+        element.bind('keypress', function(e) {
+            if (e.charCode === 13 || e.keyCode ===13 ) {
+              scope.$apply(attrs.ngEnter);
+            }
+        });
+    };
+});
+
+function Ctrl($scope) {
+    $scope.options = [
+        {text: "Johnny", checked: true},
+        {text: "Bbb", checked: true},
+        {text: "Ccc", checked: true}
+    ];
+
+    $scope.selectedOptions = function() {
+        var selValues = [];
+        _.each($scope.options, function(option) {
+            if (option.checked) {
+                selValues.push(option.text);
+            }
+        });
+        return selValues;
+    };
+
+    $scope.addOption = function() {
+        $scope.options.push({
+            text: $scope.optionText,
+            checked: true
+        });
+
+        var wiserEvent = new CustomEvent("wiserEvent",
+          {
+            "detail":
+            {
+              "eventType": "Adding to-do item",
+              "item": $scope.optionText
+            },
+            bubbles: false
+
+        });
+        $('body').broadcast(wiserEvent);
+
+        $scope.optionText = '';
+    };
+}
